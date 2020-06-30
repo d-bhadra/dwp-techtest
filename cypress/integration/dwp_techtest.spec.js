6a842368a9f18daf60c6a3bdbd2a3ff99d1ff8bf
d-bhadra <debjyoti.bhadra@gmail.com>
2020-06-30 13:00:38 +0100

    test

---
+++ cypress/integration/dwp_techtest.spec.js
@@ -0,0 +1,48 @@
+context('DWP API test - witout loading the page', () => {
+    it('cy.request() with query parameters', () => {
+        cy.request("http://bpdts-test-app-v2.herokuapp.com/instructions")
+            .then((response) => {
+                // response validation
+                expect(response).property('status').to.equal(200)
+                expect(response).property('body').to.have.property('todo')
+            })
+        cy.request('http://bpdts-test-app-v2.herokuapp.com/users')
+            .should((response) => {
+                expect(response.status).to.eq(200)
+                // expect(response.body).to.have.property('have.length', 1000)
+                expect(response).to.have.property('headers')
+                expect(response).to.have.property('duration')
+
+            })
+        cy.request('http://bpdts-test-app-v2.herokuapp.com/user/2')
+            .then((response) => {
+                // response validation
+                expect(response).property('status').to.equal(200)
+                expect(response).property('body').to.have.property('id')
+                expect(response.body).to.have.property('first_name')
+                expect(response.body).to.have.property('last_name')
+                expect(response.body).to.have.property("email")
+                expect(response.body).to.have.property("ip_address")
+                expect(response.body).to.have.property("latitude")
+                expect(response.body).to.have.property("longitude")
+                expect(response.body).to.have.property("city")
+            })
+        cy.request('http://bpdts-test-app-v2.herokuapp.com/city/Kundung/users')
+            .then((response) => {
+                // response validation
+                expect(response).property('status').to.equal(200)
+            })
+    })
+
+    it('DWP API test - Using page and page elements', () => {
+        cy.visit('http://bpdts-test-app-v2.herokuapp.com/')
+        cy.get('#operations-tag-default').click()
+
+        cy.get('#operations-default-get_users3 > .opblock-summary').click()
+        cy.get('.btn').click()
+
+        cy.get('input').type("2")
+        cy.get('.execute-wrapper > .btn').click()
+        cy.get(':nth-child(1) > .responses-table > tbody > .response > .response-col_status').should('have.text', '200')
+    })
+})
